cc7cd4b94823f2b3c20e67cc67c2724bc831aa1c
lexsilva1 <jagmsilva@student.dei.uc.pt>
2024-02-07 16:06:14 +0000

    server online

--- my_scrum_backend/src/main/java/dto/Task.java
+++ my_scrum_backend/src/main/java/dto/Task.java
@@ -1,33 +1,52 @@
 package dto;
+import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
-import jakarta.xml.bind.annotation.XmlType;
-import jakarta.json.bind.annotation.JsonbAnnotation;
-import jakarta.json.bind.annotation.JsonbProperty;
-import jakarta.json.bind.annotation.JsonbTransient;
+@XmlRootElement(name = "Task")
 public class Task {
     String Id;
     String Title;
     String Description;
+    String Status;
+    String Priority;
     public Task() {
     }
-    public Task(String id, String title, String description) {
+    public Task(String id, String title, String description, String priority) {
         Id = id;
         Title = title;
         Description = description;
+        Status = "todo";
+        Priority = priority;
     }
+    @XmlElement
     public String getId() {
         return Id;
     }
+    @XmlElement
     public String getTitle() {
         return Title;
     }
     public void setTitle(String title) {
         Title = title;
     }
+    @XmlElement
     public String getDescription() {
         return Description;
     }
     public void setDescription(String description) {
         Description = description;
+    }
+    @XmlElement
+    public String getStatus() {
+        return Status;
+    }
+    public void setStatus(String status) {
+        Status = status;
+    }
+    @XmlElement
+    public String getPriority() {
+        return Priority;
+    }
+    public void setPriority(String priority) {
+        Priority = priority;
     }
 }
