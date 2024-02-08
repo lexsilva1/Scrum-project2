2823e5700872b0e3b62732cb4c5c580135024634
Pedro Monteiro <pmmonteiro@student.dei.uc.pt>
2024-02-08 09:53:16 +0000

    edição registo

--- Frontend/registry.js
+++ Frontend/registry.js
@@ -9,17 +9,45 @@ let newUser={
 //funcionalidade dos botoes 
 document.getElementById('submitRegistryButton').addEventListener('click',()=>{
     newUser.FirstName = document.getElementById('userFirstName').value.trim();
-    newUser.LastName = document.getElementById('userLastName').value;
-    newUser.Email = document.getElementById('userEmail').value;
-    newUser.Username = document.getElementById('userUsername').value;
-    newUser.Password = document.getElementById('userPassword').value;
-    for (const [Key, value] of Object.entries(newUser)){
-        if (value == ''){
-            alert (Key + ' is empty');
-            break;
+    newUser.LastName = document.getElementById('userLastName').value.trim();
+    newUser.Email = document.getElementById('userEmail').value.trim();
+    newUser.Username = document.getElementById('userUsername').value.trim();
+    newUser.Password = document.getElementById('userPassword').value.trim();
+    const rewritePassword = document.getElementById('userRewrittenPassword').value.trim();
+    const allFieldsFilled = Object.values(newUser).every(value => value != "");
+    const passwordMatch = newUser.Password === rewritePassword;
+
+    
+    //ver se é preferivel fazer um loop pelos valores e identificar qual dos campos nao esta preenchido
+    //const emptyFields = []
+    /*for [key,value] of Object.entries(newUser){
+        if (value === ''){
+            emptyFields.push(key);
         }
-        window.location.href='index.html';
     }
+    if (emptyFields===0){
+        console.log('submit the form')
+    } else {
+        if (emptyFields > 0){
+            console.log('Please fill the following fields : ${emptyFields.join(',')})');
+        }
+    }
+
+        */
+        
+    
+    if (allFieldsFilled && passwordMatch){
+        console.log('all fields are filled, submit new user');
+        window.location.href='index.html'
+    } else {
+        if(!allFieldsFilled){
+            console.log("please fill all fields");
+        }
+        if(!passwordMatch){
+            console.log("password does not match, please re-enter your password");
+        }
+    }
+
 })
 //carregar cancel leva à pagina login
 document.getElementById('cancelRegistryButton').addEventListener('click',()=>{
