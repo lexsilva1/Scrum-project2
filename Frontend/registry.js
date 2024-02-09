c74eb3fd83b0e8e9f62634c81324ab8836086cfe
Pedro Monteiro <pmmonteiro@student.dei.uc.pt>
2024-02-09 15:06:48 +0000

    verificação users

--- Frontend/registry.js
+++ Frontend/registry.js
@@ -12,12 +12,94 @@ document.getElementById('submitRegistryButton').addEventListener('click',(e)=>{
         password : document.getElementById('userPassword').value.trim(),
         }
         console.log(JSON.stringify(newUser));
+
+        //verificação frontend de credenciais
+        const emptyFields = [];
+        for (const [key,value] of Object.entries(newUser)){
+            if (value.trim()===''){
+                emptyFields.push(key);
+            }
+        }
+        if (emptyFields.length>0){
+            for (const key of emptyFields){
+            console.log('Field ' + key + ' needs to be filled'); 
+            }
+        }
+        //password checks with rewritten password 
+        const passwordCheck = newUser.password === document.getElementById('userRewrittenPassword').value;
+        if (passwordCheck){
+            console.log("password checks out");
+        }
+        console.log(emptyFields);    
+        nameIsBlank(newUser);
+        passwordIsBlank(newUser);
+        emailIsBlank(newUser);
         checkUsername(newUser);
         //postUser(newUser);
         //window.location.href='index.html';
     
 })
 })
+async function passwordIsBlank(newUser){
+    try{
+        await fetch ('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/passwordIsBlank',{
+        method: 'POST',
+        headers:{
+            'Accept': '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+    }).then(function(response){
+        console.log(response.status)
+        if (response.status == 404){
+            alert ("password field is empty");
+        }
+    })
+    } 
+    catch(error){
+        console.log('error:', error);
+    }
+}
+async function emailIsBlank(newUser){
+    try{
+        await fetch ('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/emailIsBlank',{
+        method: 'POST',
+        headers:{
+            'Accept': '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+    }).then(function(response){
+        console.log(response.status)
+        if (response.status == 404){
+            alert ("email field is empty");
+        }
+    })
+    } 
+    catch(error){
+        console.log('error:', error);
+    }
+}
+async function nameIsBlank(newUser){
+    try{
+        await fetch ('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/nameIsBlank',{
+        method: 'POST',
+        headers:{
+            'Accept': '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+    }).then(function(response){
+        console.log(response.status)
+        if (response.status == 404){
+            alert ("name field is empty");
+        }
+    })
+    } 
+    catch(error){
+        console.log('error:', error);
+    }
+}
 async function checkUsername(newUser){
     try{
         await fetch ('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/checksUsername',{
