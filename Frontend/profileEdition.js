057a53e96b9d032d9fa79348d30ce72714047700
Pedro Monteiro <pmmonteiro@student.dei.uc.pt>
2024-02-15 14:08:37 +0000

    edição user

--- Frontend/profileEdition.js
+++ Frontend/profileEdition.js
@@ -6,28 +6,82 @@
 //fazer uma função que faça fetch ao objeto de utilizador
 //preencher os campos com os atributos do objeto que se foi buscar pelo fetch
 //quando se submete as novas informações, alterar os dados do objeto pelo patch do username 
-window.onload = function(){
-    getUserData();
-     
+window.onload = async function(){
+    let user = await getUserData();    
+    const nomes = user.name.split(" ");
+    document.getElementById('editFirstName').value = nomes[0];
+    document.getElementById('editLastName').value = nomes[1];
+    document.getElementById('profileImage').src = user.userPhoto;
+    document.getElementById('photoUpload').value = user.userPhoto;
+    document.getElementById('editUserEmail').value = user.email;
+    document.getElementById('editUserContact').value= user.contactNumber;
+    document.getElementById('oldPassword').value = user.password;
+    document.getElementById('username').value = user.username;
+
+    document.getElementById('buttonSubmitData').addEventListener('click',()=>{
+        user = {
+        name : document.getElementById('editFirstName').value.trim()+' '+document.getElementById('editLastName').value.trim(),
+        email: document.getElementById('editUserEmail').value.trim(),
+        password : document.getElementById('editNewPassword').value.trim(),
+        contactNumber : document.getElementById('editUserContact').value.trim(),
+        userPhoto : document.getElementById("profileImage").src = document.getElementById('photoUpload').value.trim(),
+        }
+        updateUserData(user);
+        console.log(user);
+    })
 }
+
+
+
 
 async function getUserData(){
     try{
         const response = await fetch(`http://localhost:8080/lexsilva-pedromont-proj2/rest/user/${sessionStorage.getItem('username')}`);
         if (!response.ok){
         throw new Error ('failed to fetch user data');
+        }
+        const obj = await response.json();
+        return obj;
+    } catch (error){
+        console.error('something went wrong', error);
+        throw error;
     }
-    const obj = await response.json();
-    console.log(obj);
-    document.getElementById('firstName').textContent = obj.name;
-    document.getElementById('profileImage').src = obj.userPhoto;
-    document.getElementById('username').innerHTML = obj.username;
-    return obj.userPhoto;
-} catch (error){
-    console.error('something went wrong', error);
-    throw error;
 }
+
+
+async function updateUserData(user){//chama o user aqui
+    try{
+    const response = await fetch(`http://localhost:8080/lexsilva-pedromont-proj2/rest/user/update`,{
+        method: 'PUT',
+        headers:{
+            'Accept':'*/*',
+            'Content-Type':'application/json',
+            'username': sessionStorage.getItem('username'),
+            'password': sessionStorage.getItem('password'),
+        },
+        body:JSON.stringify(user),
+    }).then(function(response){
+        console.log(response.status);
+        if (response.status ==404){
+            console.log('username not found')
+        } else if(response.status == 405){
+            console.log('forbidden due to header params')
+        } else if(response.status == 400){
+            console.log('failed, user not updated')
+        } else if(response.status == 200){
+            console.log('user updated sucessfully')
+        }
+    })
+    } catch(error){
+        console.error('error',error);
+    }
+
+
 }
+//abrir modal para confirmar alterações 
 document.getElementById('buttonCancelEdition').addEventListener('click',()=>{
     window.location.href='home.html';
 })
+function fieldVerification(){
+
+}
