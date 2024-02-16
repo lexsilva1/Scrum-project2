229aff381d5c5e046e6c978b7a3647ff6309f78e
Pedro Monteiro <pmmonteiro@student.dei.uc.pt>
2024-02-16 16:09:57 +0000

    funcoes dentro do ficheiro appTestFunct

--- Frontend/appTestFunct.js
+++ Frontend/appTestFunct.js
@@ -1,28 +1,4 @@
-//Funcoes registo
-async function postUser(newUser){
-    // Send POST request with newUser data
-     try {
-         await fetch('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/add',{
-            method: 'POST',
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newUser),
-        }).then(function (response) {
-            if (response.status == 200) {
-            alert('user is added successfully :)');
-            } else {
-            alert('something went wrong :(');
-            console.log(response.status);
-            }
-            });
-}
-catch (error) {
-    console.error('Error:', error);
-}
-}
-//funcoes scrum
+//funcoes homescript.js
 function createTask(name, description, priority,startdate,enddate) { // Cria uma tarefa com o nome, a descrição e a priority passados como argumentos 
     const task = {
     title :name,
@@ -33,23 +9,38 @@ function createTask(name, description, priority,startdate,enddate) { // Cria uma
     }
     return task;
   }
-async function postTask(task) {
-    await fetch('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/addtask', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'username': sessionStorage.getItem('username'),
-        'password': sessionStorage.getItem('password')
-      },
-      body: JSON.stringify(task)
-    }).then(function(response){
-      if (response.status === 200){
-        alert('Task added');
-      }else if (response.status === 404){
-        alert('User not found');
-      }
-    });
+function setPriorityButtonSelected(button, priority) {
+    const buttons = [lowButton, mediumButton, highButton];
+    buttons.forEach(btn => btn.classList.remove("selected"));
+    button.classList.add("selected");
+    taskPriority = priority;
   }
+  function removeSelectedPriorityButton() {
+    const buttons = [lowButton, mediumButton, highButton];
+    buttons.forEach(btn => btn.classList.remove("selected"));
+  }
+  function getDragAfterElement(panel, y) {
+    const draggableElements = [...panel.querySelectorAll('.task:not(.dragging)')] // Dentro da lista de painéis, seleciona todos os elementos com a classe task que nao tenham a classe dragging  
+    return draggableElements.reduce((closest, child) => { // Retorna o elemento mais próximo do que esáa a ser arrastado e que está a ser comparado
+        const box = child.getBoundingClientRect() // Retorna o tamanho do elemento e a sua posição relativamente ao viewport
+        const offset = y - box.top - box.height / 2 // Calcula a distância entre o elemento que está a ser arrastado e o que está a ser comparado
+        if (offset < 0 && offset > closest.offset) { // Se a distância for menor que 0 e maior que a distância do elemento mais próximo até agora
+            return { offset: offset, element: child }
+        } else { //
+            return closest // Retorna o elemento mais próximo até agora
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element} 
+
+function createTask(name, description, priority,startdate,enddate) { // Cria uma tarefa com o nome, a descrição e a priority passados como argumentos 
+        const task = {
+        title :name,
+        description: description,
+        priority: priority,
+        startDate: startdate,
+        endDate: enddate,
+        }
+        return task;
+      }
 function createTaskElement(task) {
     const taskElement = document.createElement('div');
     taskElement.id = task.id;
@@ -121,117 +112,11 @@ function createTaskElement(task) {
 
     return taskElement;
 }
-async function loadTasks() {
-
-    await fetch('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/tasks', {
-     method: 'GET',
-     headers: {
-       'Content-Type': 'application/json',
-       'username': sessionStorage.getItem('username'),
-       'password': sessionStorage.getItem('password')
-     }
-   }).then(async function(response){
-     if (response.status === 200){
-       const taskArray = await response.json();
-       
-       if (taskArray.length > 0) {
-         taskArray.forEach(task => {
-           const taskElement = createTaskElement(task);
-           if (task.status === 10) {
-             document.getElementById('todo').appendChild(taskElement);
-           } else if (task.status === 20) {
-             document.getElementById('doing').appendChild(taskElement);
-           }else if (task.status === 30) {
-             document.getElementById('done').appendChild(taskElement);
-           }
-           attachDragAndDropListeners(taskElement);
-         });
-       }
-       
-     }else if (response.status === 404){
-       alert('User not found');
-     }else if (response.status === 401){
-       alert('Unauthorized');
-     }
-   });
- }
-async function deleteTask(id) {
-    try {
-      const response = await fetch('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/removetask', {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          'username': sessionStorage.getItem('username'),
-          'password': sessionStorage.getItem('password'),
-          'id': id
-        }
-      });
-  
-      if (response.ok) {
-        alert('Task deleted');
-      } else if (response.status === 404) {
-        alert('Task not found');
-      } else if (response.status === 401) {
-        alert('Unauthorized');
-      } else {
-        // Handle other response status codes
-        console.error('Unexpected response:', response.status);
-      }
-    } catch (error) {
-      console.error('Error deleting task:', error);
-      // Handle fetch errors
-      alert('Error deleting task. Please try again.');
-    }
-  }
-async function updateTask(taskElement) {
-    console.log(taskElement.startDate);
-    console.log(taskElement.endDate);
-   let taskElementstatus;
-    if(taskElement.status === "todo"){
-      taskElementstatus = 10
-    }else if(taskElement.status === "doing"){
-      taskElementstatus = 20
-    }else if(taskElement.status === "done"){
-      taskElementstatus = 30
-    }
-    let task = {
-      id: taskElement.id,
-      title: taskElement.title,
-      description: taskElement.description,
-      priority: taskElement.priority,
-      startDate: taskElement.startdate,
-      endDate: taskElement.enddate,
-      status: taskElementstatus
-    };
-  
-    try {
-      const response = await fetch('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/updatetask', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          'username': sessionStorage.getItem('username'),
-          'password': sessionStorage.getItem('password')
-        },
-        body: JSON.stringify(task)
-      });
-  
-      if (response.ok) {
-        console.log('Task updated');
-      } else if (response.status === 404) {
-        console.log('Task not found');
-      } else if (response.status === 401) {
-        console.log('Unauthorized');
-      } else {
-        // Handle other response status codes
-        console.error('Unexpected response:', response.status);
-      }
-    } catch (error) {
-      console.error('Error updating task:', error);
-      // Handle fetch errors
-      alert('Error updating task. Please try again.');
-    }
-
-
+function showTime() {
+    let time = new Date();
+    let timeString = time.toLocaleTimeString("en-US", { hour12: false, hour: '2-digit', minute: '2-digit' });
+    displayTime.innerText = timeString;
+    setTimeout(showTime, 1000);
   }
 function updateDate() { // Mostra a data atual
     let today = new Date();
@@ -272,133 +157,18 @@ function updateDate() { // Mostra a data atual
       document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i]; // Altera o valor do elemento html com o ID correspondente
     }
   }
-//funcoes login
-async function login(loginValue, passwordValue) {
-    // Send GET request with username and password as query parameters
-    try {
-        await fetch(`http://localhost:8080/lexsilva-pedromont-proj2/rest/user/login?username=${loginValue}&password=${passwordValue}`, {
-            method: 'GET',
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'application/json',
-                username: loginValue,
-                password: passwordValue,
-            }
-        }).then(async function(response) {
-            if (response.status === 200) {
-                // User is logged in successfully
-                alert('User is logged in successfully :)');
-                const userData = await response.json();
-                
-                // Store user data in sessionStorage
-                sessionStorage.setItem('password', userData.password);
-                sessionStorage.setItem('username', userData.username);
-                // Add other user properties as needed
-                
-                // Redirect to index.html after successful login
-                window.location.href = 'home.html';
-            } else if (response.status === 404) {
-                // User not found
-                alert('User not found');
-            } else {
-                // Something went wrong
-                alert('Something went wrong :(');
-            }
-        });
-    } catch (error) {
-        console.error('Error:', error);
-    }
-
+//funcoes task.js
+function setStatusButtonSelected(button, status) {
+    const buttons = [todoButton, doingButton, doneButton];
+    buttons.forEach(btn => btn.classList.remove("selected"));
+    button.classList.add("selected");
+    sessionStorage.setItem("taskStatus", status);
 }
-//funcoes task
-async function updateTask() {
-    let taskElementstatus=sessionStorage.getItem("taskStatus");
 
-    if(taskElementstatus === "todo"){
-      taskElementstatus = 10
-    }else if(taskElementstatus === "doing"){
-      taskElementstatus = 20
-    }else if(taskElementstatus === "done"){
-      taskElementstatus = 30
-    }
-     let task = {
-         id: sessionStorage.getItem("taskid"),
-         title: document.getElementById("titulo-task").value.trim(),
-         description: document.getElementById("descricao-task").value.trim(),
-         status: taskElementstatus,
-         priority: sessionStorage.getItem("taskPriority"),
-         startDate: document.getElementById('startdate').value,
-         endDate: document.getElementById('enddate').value
-
-      };
-   
-     try {
-       const response = await fetch('http://localhost:8080/lexsilva-pedromont-proj2/rest/user/updatetask', {
-         method: 'PUT',
-         headers: {
-           'Content-Type': 'application/json',
-           'username': sessionStorage.getItem('username'),
-           'password': sessionStorage.getItem('password')
-         },
-         body: JSON.stringify(task)
-       });
-   
-       if (response.ok) {
-         console.log('Task updated');
-       } else if (response.status === 404) {
-         console.log('Task not found');
-       } else if (response.status === 401) {
-         console.log('Unauthorized');
-       } else {
-         // Handle other response status codes
-         console.error('Unexpected response:', response.status);
-       }
-     } catch (error) {
-       console.error('Error updating task:', error);
-       // Handle fetch errors
-       alert('Error updating task. Please try again.');
-     }
-   }
-//Funcoes editar perfil 
-async function updateUserData(user){//chama o user aqui
-    try{
-    const response = await fetch(`http://localhost:8080/lexsilva-pedromont-proj2/rest/user/update`,{
-        method: 'PUT',
-        headers:{
-            'Accept':'*/*',
-            'Content-Type':'application/json',
-            'username': sessionStorage.getItem('username'),
-            'password': sessionStorage.getItem('password'),
-        },
-        body:JSON.stringify(user),
-    }).then(function(response){
-        console.log(response.status);
-        if (response.status ==404){
-            console.log('username not found')
-        } else if(response.status == 405){
-            console.log('forbidden due to header params')
-        } else if(response.status == 400){
-            console.log('failed, user not updated')
-        } else if(response.status == 200){
-            console.log('user updated sucessfully')
-        }
-    })
-    } catch(error){
-        console.error('error',error);
-    }
-
-
-}
-async function getUserData(){
-    try{
-        const response = await fetch(`http://localhost:8080/lexsilva-pedromont-proj2/rest/user/${sessionStorage.getItem('username')}`);
-        if (!response.ok){
-        throw new Error ('failed to fetch user data');
-        }
-        const obj = await response.json();
-        return obj;
-    } catch (error){
-        console.error('something went wrong', error);
-        throw error;
-    }
+// Função para definir o estado no grupo de botões priority
+function setPriorityButtonSelected(button, priority) {
+    const buttons = [lowButton, mediumButton, highButton];
+    buttons.forEach(btn => btn.classList.remove("selected"));
+    button.classList.add("selected");
+    sessionStorage.setItem("taskPriority", priority);
 }
